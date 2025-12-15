eceed5d4185afd66b1da065cefba024c13388863
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-12-15 14:14:48 -0600

    Fix school object property access in index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let school = {
-  name: 'Daleville';
-  'average class size': 12;
-  mascot: 'Bird';
-}
+  name: 'Daleville',
+  'average class size': 12,
+  mascot: 'Bird',
+};
 
-document.getElementById('result').innerHTML = `${name} has an average class size of ${average class size} and their mascot is a ${mascot}`;
+document.getElementById('result').innerHTML = `${school.name} has an average class size of ${school['average class size']} and their mascot is a ${school.mascot}`;
+
