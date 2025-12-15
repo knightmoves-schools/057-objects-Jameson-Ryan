83bf62aac0f9cc187a07fde4f60fe1130195e196
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-12-15 14:11:43 -0600

    Add school object and display its properties
    Fixed syntax errors in the school object and updated the output string.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let school = {
+  name: 'Daleville';
+  'average class size': 12;
+  mascot: 'Bird';
+}
 
+document.getElementById('result').innerHTML = `${name} has an average class size of ${average class size} and their mascot is a ${mascot}`;
